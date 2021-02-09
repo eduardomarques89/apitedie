c6837b8727202cf2f47dd9f1f333d0976dbdfd32
Otávio C. Oliveira <otaviool@hotmail.com>
2021-02-08 22:12:48 -0300

    fechar pedido

--- apitedie/Controllers/ClientesController.cs
+++ apitedie/Controllers/ClientesController.cs
@@ -34,7 +34,7 @@ namespace apitedie.Controllers
 
             try
             {
-                if(cliente == null)
+                if (cliente == null)
                 {
                     return Request.CreateResponse(HttpStatusCode.BadRequest, "Cliente não informado.");
                 }
@@ -101,7 +101,7 @@ namespace apitedie.Controllers
 
             List<Clientes> clientes = (List<Clientes>)repositorio.GetAll();
             Clientes cliente = clientes.Find(c => c.Email.ToLower() == login.ToLower());
-            cliente.Enderecos = null;            
+            cliente.Enderecos = null;
             try
             {
                 var token = tokenRepo.GeraToken(cliente);
@@ -111,6 +111,76 @@ namespace apitedie.Controllers
             {
                 return Request.CreateResponse(HttpStatusCode.Created, ex.Message);
             }
+        }
+
+        [Route("api/clientes/PostCartao")]
+        public HttpResponseMessage PostCartao(CartaoCredito card)
+        {
+            SqlConnection _conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+
+            _conn.Open();
+            SqlCommand _comandoSQL = new SqlCommand(
+                            " INSERT INTO APP_CARTAO (idcliente, numero, datavenc, cpf, nome, cvv, idbandeira) " +
+                            " output INSERTED.IDCARTAO VALUES (@idcliente, @numero, @datavenc, @cpf, @nome, @cvv, @idbandeira) ", _conn);
+
+            _comandoSQL.Parameters.AddWithValue("@idcliente", card.IdCliente);
+            _comandoSQL.Parameters.AddWithValue("@numero", card.Numero);
+            _comandoSQL.Parameters.AddWithValue("@datavenc", card.Validade);
+            _comandoSQL.Parameters.AddWithValue("@cpf", card.CPF);
+            _comandoSQL.Parameters.AddWithValue("@nome", card.Titular);
+            _comandoSQL.Parameters.AddWithValue("@cvv", card.CVV);
+            _comandoSQL.Parameters.AddWithValue("@idbandeira", card.IdBandeira);
+
+            int idCartao = (int)_comandoSQL.ExecuteScalar();
+            if (idCartao > 0)
+                return Request.CreateResponse(HttpStatusCode.Created);
+            else
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+        }
+
+        [Route("api/clientes/GetCartao")]
+        public HttpResponseMessage GetCartao(int idCliente)
+        {
+            List<CartaoCredito> cartoes = new List<CartaoCredito>();
+            SqlConnection _conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+
+            SqlCommand _comandoSQL = new SqlCommand(
+                            " SELECT datavenc, idcartao, idcliente, numero, b.idbandeira, cpf, nome, cvv, nomebandeira" +
+                            " FROM APP_CARTAO c LEFT JOIN app_bandeira B ON B.idbandeira= c.idbandeira" +
+                            " WHERE idCliente= @idCliente", _conn);
+
+            _comandoSQL.Parameters.AddWithValue("@idCliente", idCliente);
+
+            try
+            {
+                _conn.Open();
+                var dr = _comandoSQL.ExecuteReader();
+                while (dr.Read())
+                {
+                    cartoes.Add(new CartaoCredito
+                    {
+                        IdCliente = dr["idcliente"].ToString() == "" ? 0 : Convert.ToInt32(dr["idcliente"]),
+                        Numero = dr["numero"].ToString(),
+                        IdCartao = dr["idCartao"].ToString() == "" ? 0 : Convert.ToInt32(dr["idCartao"]),
+                        Validade = dr["datavenc"].ToString(),
+                        CPF = dr["cpf"].ToString(),
+                        Titular = dr["nome"].ToString(),
+                        CVV = dr["cvv"].ToString(),
+                        IdBandeira = dr["idbandeira"].ToString(),
+                        Bandeira = dr["nomebandeira"].ToString(),
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+            finally
+            {
+                _conn.Close();
+            }
+
+            return Request.CreateResponse(HttpStatusCode.Created, cartoes);
         }
     }
 }
---
+++ apitedie/Models/CartaoCredito.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace apitedie.Models
+{
+    public class CartaoCredito
+    {
+        public int IdCliente { get; set; }
+        public int IdCartao { get; set; }
+        public string Token { get; set; }
+        public string Numero { get; set; }
+        public string IdBandeira { get; set; }
+        public string Validade { get; set; }
+        public string CPF { get; set; }
+        public string CVV { get; set; }
+        public string Titular { get; set; }
+        public string Bandeira { get; set; }
+    }
+}
