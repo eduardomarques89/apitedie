c26facf90f4ae124714f5d760ce40c9c48d54226
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-04-22 20:29:28 -0300

    add integration of coupons

--- tedie-mobile/src/screens/Coupons.js
+++ tedie-mobile/src/screens/Coupons.js
@@ -1,37 +1,39 @@
 import React, {
-  useState, useCallback, useEffect, useContext,
+  useState, useCallback, useContext,
 } from 'react';
-import { TouchableOpacity, StatusBar } from 'react-native';
+import { TouchableOpacity, StatusBar, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 // components
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import theme from '../theme';
 import Navbar from '../components/Navbar';
-import ScreenContainer from '../components/ScreenContainer';
 import Typography from '../components/Typography';
 import CouponItem from '../components/CouponItem';
+import Loader from '../components/Loader';
 // services
-import { getCoupons } from '../services/coupons';
+import api from '../services/axios';
 import { CheckoutContext } from '../contexts/CheckoutContext';
+import { AppContext } from '../contexts/AppContext';
 
 const Coupons = ({ navigation }) => {
   const navigate = useNavigation();
   const [couponsLoader, setCouponsLoader] = useState(false);
   const [coupons, setCoupons] = useState([]);
   const { checkoutState, checkoutDispatch } = useContext(CheckoutContext);
+  const { state } = useContext(AppContext);
 
   const loadCoupons = useCallback(async () => {
     setCouponsLoader(true);
 
-    const couponsResponse = await getCoupons();
-    setCoupons(couponsResponse);
+    const couponsResponse = await api(`CuponsCliente/${state?.sessao?.IdCliente}`);
+    setCoupons(couponsResponse.data);
 
     setCouponsLoader(false);
-  }, [setCouponsLoader, setCoupons, getCoupons]);
+  }, []);
 
-  useEffect(() => {
+  useFocusEffect(useCallback(() => {
     loadCoupons();
-  }, [loadCoupons]);
+  }, [loadCoupons]));
 
   async function selecionaCupom(cupom) {
     const action = { type: 'setCupom', payload: { cupom } };
@@ -63,18 +65,21 @@ const Coupons = ({ navigation }) => {
           </Typography>
         )}
       />
-
-      <ScreenContainer>
-        {coupons.length > 0 && !couponsLoader
-          && coupons.map((coupon) => (
-            <TouchableOpacity onPress={() => selecionaCupom(coupon)}>
-              <CouponItem
-                key={coupon.IdCupom}
-                coupon={coupon}
-              />
-            </TouchableOpacity>
-          ))}
-      </ScreenContainer>
+      <Loader show={couponsLoader} />
+      {!couponsLoader && (
+      <FlatList
+        contentContainerStyle={{ margin: 16 }}
+        data={coupons}
+        keyExtractor={(item) => `${item.IdCupom}`}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => selecionaCupom(item)}>
+            <CouponItem
+              coupon={item}
+            />
+          </TouchableOpacity>
+        )}
+      />
+      )}
     </>
   );
 };
